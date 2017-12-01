6ffe9611e853bfae31b7a2ec3440fc0a52d1d7db
Drew Barrett <dbarret1@cs.umbc.edu>
2017-12-01 14:20:04 -0500

    pt2

--- day1/p1.py
+++ day1/p1.py
@@ -10,11 +10,13 @@ direction = NORTH
 
 file = open("input.txt")
 file = file.readlines()
+
+hasBeen = []
+
 for line in file:
     commands = line.split(", ")
     for command in commands:
         mod = 0
-        print(command)
         dir = command[0]
         if dir == "R":
             mod = 1
@@ -23,6 +25,8 @@ for line in file:
         direction += mod
         direction %= 4
         distance = int(command[1:])
+        prevX = x
+        prevY = y
         if direction == NORTH:
             y += distance
         elif direction == EAST:
@@ -31,5 +35,25 @@ for line in file:
             y -= distance
         elif direction == WEST:
             x -= distance
+        
+        def insert(a,b):
+            if (a,b) in hasBeen:
+                print(a,b)
+            else:
+                hasBeen.append((a,b))
 
-print(x, y)
+        if x != prevX:
+            for i in range(1, abs(x - prevX)):
+                if x - prevX > 0:
+                    insert(prevX + i,y)
+                else:
+                    insert(prevX - i,y)
+        if y != prevY:
+            for i in range(1, abs(y - prevY)):
+                if y - prevY > 0:
+                    insert(x,prevY + i)
+                else:
+                    insert(x,prevY - i)
+
+
+print(abs(x) + abs(y))
